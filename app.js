6484df494ad6dcd89283bddc407192aab636e720
profgrammer <javarep4199@gmail.com>
2019-12-11 22:46:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const fetch = require('node-fetch');
+app.use(bp.json());
+
+app.post('/dialogflow', (req, res) => {
+    fetch('https://yesno.wtf/api').then(result => {
+        result.json().then(body => {
+            res.json({
+                "fulfillmentMessages": [
+                  {
+                    "image": {
+                      "imageUri": body.image
+                    }
+                  }
+                ],
+              });
+        }).catch(error => {
+            res.json({
+                "fulfillmentMessages": [
+                    {
+                        "text": "unable to reach the server."
+                    }
+                ]
+            })
+        });
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
